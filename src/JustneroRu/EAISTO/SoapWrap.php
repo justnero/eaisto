71c18c268bda822547fa5191aaf07cbd3ec6dc42
JustNero <justnero.ru@yandex.ru>
2017-08-01 23:36:36 +0300

    Make SoapClient throw exception instead of fatal error when xDebug in use See https://bugs.php.net/bug.php?id=47584 (Yea, it`s 8 years old bug)

--- src/JustneroRu/EAISTO/SoapWrap.php
+++ src/JustneroRu/EAISTO/SoapWrap.php
@@ -26,28 +26,45 @@ class SoapWrap extends \SoapClient {
 			}
 		}
 		$options = array_merge( [
-			'features' => 1,
+			'features'   => 1,
+			'exceptions' => 1,
+			'cache_wsdl' => WSDL_CACHE_NONE,
 		], $options );
 		if ( ! $wsdl ) {
 			$wsdl = static::$wsdl;
 		}
-		try {
-			parent::__construct( $wsdl, $options );
-		} catch ( \SoapFault $ex ) {
-			parent::__construct( $this->getCache( $cacheDir, $wsdl ), $options );
+		if ( function_exists( 'xdebug_disable' ) ) {
+			xdebug_disable();
+		}
+		$done = true;
+		do {
+			unset( $this->__soap_fault );
+			try {
+				@parent::__construct( $this->getCache( $cacheDir, $wsdl, ! $done ), $options );
+				$done = true;
+			} catch ( \SoapFault $ex ) {
+				$done = false;
+			}
+			if ( isset( $this->__soap_fault ) ) {
+				$done = false;
+			}
+		} while ( ! $done );
+		if ( function_exists( 'xdebug_enable' ) ) {
+			xdebug_enable();
 		}
 	}
 
 	/**
 	 * @param string $cacheDir path to a directory to store cache
 	 * @param string $wsdl The wsdl file to use
+	 * @param bool $force Reload file anyway
 	 *
 	 * @return string path to a cached copy of wsdl
 	 */
-	private function getCache( $cacheDir, $wsdl ) {
-		$filename = hash( 'md5', $wsdl );
+	private function getCache( $cacheDir, $wsdl, $force = false ) {
+		$filename = hash( 'md5', $wsdl ) . '.wsdl';
 		$path     = rtrim( $cacheDir, DIRECTORY_SEPARATOR ) . DIRECTORY_SEPARATOR . $filename;
-		if ( ! file_exists( $path ) || filemtime( $path ) < time() - 86400 ) {
+		if ( $force || ! file_exists( $path ) || filemtime( $path ) < time() - 3600 ) {
 			do {
 				$result = file_put_contents( $path, fopen( $wsdl, 'r' ) );
 			} while ( ! $result );
